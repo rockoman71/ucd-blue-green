c1b4654dde4aeac8f6b7aace248a67989df16c43
Rodrigo de la Parra <rodrigo.delaparra@gmail.com>
2018-10-15 19:03:37 -0400

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -13,11 +13,13 @@ def hello():
            "<p><font size='6'>Hello {name}!<br/></font></p>" \
            "<p><font size='6'><b>Hostname:</b> {hostname}<br/></font></p>" \
            "<p><font size='6'><b>Environment:</b> {env}<br/></font></p>" \
+           "<p><font size='6'><b>Version:</b> {version}<br/></font></p>" \
            "</body></html>"
     return html.format(name=os.getenv("NAME", "world"), 
                        hostname=socket.gethostname(), 
                        env=os.getenv("env", "Dev"), 
-                       backgroundcolor=os.getenv("backgroundcolor", "MintCream"))
+                       backgroundcolor=os.getenv("backgroundcolor", "MintCream"),
+                       version=os.getenv("version", "n/a"))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
