99518ac2f7c2ed88ad00079ebb184bb2daad74bd
Rodrigo de la Parra <rodrigo.delaparra@gmail.com>
2018-10-18 12:18:40 -0400

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -9,7 +9,7 @@ def hello():
     html = "<html><head>" \
            "<title>IBM Demo DevOps</title></head>" \
            "<body style='background-color:{backgroundcolor};'>" \
-           "<p><font size='10'>IBM DevOps Demo - UrbanCode Deploy & IBM Cloud Private!<br/></font></p>" \
+           "<p><font size='10'>IBM DevOps Demo - UrbanCode Deploy & IBM Cloud Private!!!<br/></font></p>" \
            "<p><font size='6'>Hello {name}!<br/></font></p>" \
            "<p><font size='6'><b>Hostname:</b> {hostname}<br/></font></p>" \
            "<p><font size='6'><b>Environment:</b> {env}<br/></font></p>" \
