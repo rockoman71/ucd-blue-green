19caef614e7fa0202c1354e42cc73fd94e1cca09
Rodrigo de la Parra <rodrigo.delaparra@gmail.com>
2019-04-11 11:04:22 -0400

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -10,7 +10,7 @@ def hello():
            "<title>IBM Cloud Private + dev/test/delivery POT</title></head>" \
            "<body style='background-color:{backgroundcolor};'>" \
            "<p><font size='6'>IBM DevOps PoT - Transformation Advisor, Microclimate, UrbanCode Deploy & Velocity<br/></font></p>" \
-           "<br><font size='5'><a target='_blank' rel='noopener' href='http://conf.bluedemos.com/app/home/session/3834/7o7mdu0zli69U8ZCBTKKFWZ1UV7HZSY80hiwmolqohwfkw341k7eel6dun23gjs8'>Click here to open your DevOps ICP PoT environment</a></font><br>" \
+           "<!--<br><font size='5'><a target='_blank' rel='noopener' href='http://conf.bluedemos.com/app/home/session/3834/7o7mdu0zli69U8ZCBTKKFWZ1UV7HZSY80hiwmolqohwfkw341k7eel6dun23gjs8'>Click here to open your DevOps ICP PoT environment</a></font><br>-->" \
            "<p><font size='4'><b>Hostname:</b> {hostname}<br/></font></p>" \
            "<p><font size='4'><b>Environment:</b> {env}<br/></font></p>" \
            "<p><font size='4'><b>Version:</b> {version}<br/></font></p>" \
